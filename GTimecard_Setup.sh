bf2c17f5cfba61ea81ee1acae6597caa082f1fd5
TumoiYorozu <morojuyi+github@gmail.com>
2015-10-19 18:40:04 +0900

    名前とカレンダーIDを設定できるように

--- GTimecard_Setup.sh
+++ GTimecard_Setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-bundle install --path vendor/bundle
+ bundle install --path vendor/bundle
 
-bundle exec google-api oauth-2-login --client-id="785375802976-svic92uqburdun23ba9c067ndg33u5er.apps.googleusercontent.com" --client-secret="oMyjj58YZGPcjM_Mgzx_yeoZ" --scope="https://www.googleapis.com/auth/calendar"
+ bundle exec google-api oauth-2-login --client-id="785375802976-svic92uqburdun23ba9c067ndg33u5er.apps.googleusercontent.com" --client-secret="oMyjj58YZGPcjM_Mgzx_yeoZ" --scope="https://www.googleapis.com/auth/calendar"
 
-mv ~/.google-api.yaml .
+ mv ~/.google-api.yaml .
+
+echo 'あなたの名前を入力'
+read NAME
+echo "$NAME" > worker_name.txt
+
+echo 'カレンダーIDを入力'
+read ID
+echo "$ID" > calendar_id.txt
+
